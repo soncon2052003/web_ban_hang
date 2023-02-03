870879b0083b6f08ad3672fda8e261506fddef2b
soncon2052003 <90188775+soncon2052003@users.noreply.github.com>
2023-02-03 10:55:10 +0700

    Revert "3/2/2023 morning"
    This reverts commit 775707d394650908f0c832e24ba9676b9a07ea70.

--- cart_comment.php
+++
@@ -1,8 +0,0 @@
-<div class="card mb-3" style="max-width: 720px;">
-        <div class="row g-0">
-            <div class="card-body">
-                <h5 class="card-text"><?= $row["fullname"]; ?></h5>
-                <p class="card-text"><small class="text-muted">Noi dung: <?= $row["content"] ?></small></p>
-            </div>
-        </div>
-    </div>
--- controller/comment.php
+++ controller/comment.php
@@ -1,6 +1,5 @@
 <?php
 require "../model/comment.php";
-$_POST['title']="abc";
 $comment = new comment($_POST['fullname'],$_POST['title'],$_POST['content'],$_POST['id_sp']);
 $comment->them_comment();
 ?>
